d7c82800884419da7a9d886a16ff8055e99b1e17
Matthew van der Does <matt024601@gmail.com>
2024-10-21 13:05:06 -0400

    Added text and space transition
    Added text and space transition

--- src/teleport.gd
+++ src/teleport.gd
@@ -2,7 +2,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		body.set_position($Destination.global_position)
-		print("teleport")
+		if(self.name == "EndOfSecondRoom"):
+			get_tree().change_scene_to_file("res://src/Scenes/space.tscn")
+		else:
+			body.set_position($Destination.global_position)
+			print("teleport")
 	else:
 		print("test")
