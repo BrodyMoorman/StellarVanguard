68a35a99ec55be7af0717e90ed2634cb6e40b7a4
BrodyMoorman <115063567+BrodyMoorman@users.noreply.github.com>
2024-11-20 09:03:29 -0500

    Fixed vent jump glitch

--- src/Scripts/player/player.gd
+++ src/Scripts/player/player.gd
@@ -125,7 +125,8 @@ func _physics_process(delta: float) -> void:
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+		if(!player_hidden):
+			velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
