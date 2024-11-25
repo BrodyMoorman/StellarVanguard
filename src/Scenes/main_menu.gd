08d2dc3703bb0c58bf179faa5e36c3e60ded08a1
Thomas Sutton <157766439+thsutton159@users.noreply.github.com>
2024-11-25 10:47:24 -0500

    updated menus and menu navigation
    added all controls mentioned in the tutorial to the controls screen.
    
    added in a settings menu to control volume, still working on the setting for brightness.
    
    made sure that all of the menu's could navigate to each other properly.

--- src/Scenes/main_menu.gd
+++ src/Scenes/main_menu.gd
@@ -12,12 +12,11 @@ func _process(_delta: float) -> void:
 # loads a fresh game when clicked
 func _on_start_button_pressed() -> void: 
 	get_tree().change_scene_to_file("res://src/Scenes/Tutorial.tscn")
-	
+
+# loads the options menu when clicked
 func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/Scenes/Options_Menu.tscn")
-	#var options = preload("res://src/Scenes/Options_Menu.tscn")
-	#var scene = options.instantiate()
-	#get_tree().current_scene.add_child(scene)
 
+# quits the game
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
--- src/Scenes/options_menu.gd
+++ src/Scenes/options_menu.gd
@@ -5,14 +5,22 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-# returns to the previous screen when the back button is called
+# work in progress
+# creates the options menu as a child scene of another scene
+func _optionsStart() -> void:
+	pass
+	#var scene = preload("res://src/Scenes/Options_menu.tscn")
+
+# returns to the main menu when the back button is called
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/Scenes/Main_Menu.tscn")
 
 func _on_controls_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/Scenes/Controls_Menu.tscn")
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/Scenes/Settings_Menu.tscn")
