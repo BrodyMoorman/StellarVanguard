f1abe927097c11b03c3d942d9bcd6103a6fce51b
Matthew van der Does <matt024601@gmail.com>
2024-12-02 14:36:56 -0500

    Added Level 1, changed some other stuff

--- src/teleport.gd
+++ src/teleport.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") or body.name == "Player":
 		if(self.name == "EndOfSecondRoom"):
 			get_tree().change_scene_to_file("res://src/Scenes/space.tscn")
+		elif(self.name == "EndOfSpaceOne"):
+			get_tree().change_scene_to_file("res://src/Scenes/Levels/Level1.tscn")
+			print("a")
 		else:
 			body.set_position($Destination.global_position)
 			print("teleport")
