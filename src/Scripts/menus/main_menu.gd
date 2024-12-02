1cda748036dca95f7b2d432aba75f6e620484eed
Thomas Sutton <157766439+thsutton159@users.noreply.github.com>
2024-12-02 12:09:41 -0500

    Added back story scenes before the level select screen

--- src/Scripts/menus/main_menu.gd
+++ src/Scripts/menus/main_menu.gd
@@ -11,7 +11,7 @@ func _process(_delta: float) -> void:
 
 # loads a fresh game when clicked
 func _on_start_button_pressed() -> void: 
-	get_tree().change_scene_to_file("res://src/Scenes/level_select.tscn")
+	get_tree().change_scene_to_file("res://src/Scenes/back_story_1.tscn")
 
 # loads the options menu when clicked
 func _on_options_button_pressed() -> void:
